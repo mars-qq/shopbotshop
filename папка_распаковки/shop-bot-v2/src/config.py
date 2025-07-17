70c1009cb0982b0a9152d9ef0d29ed150a504af3
mars-qq <kakashkasashka3@gmail.com>
2025-07-17 21:45:09 +0300

    Update config.py

--- папка_распаковки/shop-bot-v2/src/config.py
+++ папка_распаковки/shop-bot-v2/src/config.py
@@ -1,43 +1,11 @@
-import configparser
 import os
+from dotenv import load_dotenv
 
-DIR = os.path.abspath(__file__)[:-14]
+load_dotenv()
 
-config = configparser.ConfigParser()
-config.read(f"{DIR}/settings.ini")
-TOKEN = config["settings"]["token"]
-COMMENT = config["settings"]["comment_pay"]
-get_id = config["settings"]["admin_id"]
-ADMIN_ID = []
-
-if "," in get_id:
-    get_id = get_id.split(",")
-    for a in get_id:
-        ADMIN_ID.append(str(a))
-else:
-    try:
-        ADMIN_ID = [str(get_id)]
-    except ValueError:
-        ADMIN_ID = [0]
-        print("Не указан Admin_ID")
-
-
-def is_admin(user_id):
-    """
-    Проверка юзера на админа
-
-    :param user_id: id юзера
-    :return: true - юзер админ, false - нет
-    """
-    return str(user_id) in ADMIN_ID
-
-
-def create_folder(src):
-    """
-    Проверка/создание папки с путём src
-
-    :param src: путь к папке
-    :return:
-    """
-    if not os.path.exists(src):
-        os.mkdir(src)
+class Config:
+    BOT_TOKEN = os.getenv('BOT_TOKEN')
+    DB_HOST = os.getenv('DB_HOST')
+    DB_USER = os.getenv('DB_USER')
+    DB_PASS = os.getenv('DB_PASS')
+    DB_NAME = os.getenv('DB_NAME')
